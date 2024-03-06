3e29502be54d0f321783bc45367d394023ccf41e
photowey <photowey@gmail.com>
2024-03-06 19:55:54 +0800

    feat: Add splashscreen

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,11 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::{thread, time};
 use chrono::{Local, NaiveDateTime};
-use chronounit::formatter::pattern::DateTimePattern;
 use chronounit::formatter::{DateTimeFormatter, DefaultDateTimeFormatter};
+use chronounit::formatter::pattern::DateTimePattern;
+use tauri::Manager;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -20,9 +22,32 @@ fn greet(name: &str) -> String {
     )
 }
 
+#[tauri::command]
+async fn close_splashscreen(window: tauri::Window) {
+    // Close splashscreen
+    if let Some(splashscreen) = window.get_window("splashscreen") {
+        splashscreen.close().unwrap();
+    }
+    // Show main window
+    window.get_window("main").unwrap().show().unwrap();
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            let splashscreen_window = app.get_window("splashscreen").unwrap();
+            let main_window = app.get_window("main").unwrap();
+            tauri::async_runtime::spawn(async move {
+
+                thread::sleep(time::Duration::from_millis(5000));
+
+                splashscreen_window.close().unwrap();
+                main_window.show().unwrap();
+            });
+
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![greet, close_splashscreen])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
